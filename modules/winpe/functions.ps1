2befac08b0c122341aa82703cd332fe55da0a79b
Your Name <david@segura.org>
2025-12-23 23:03:43 -0600

    fix(winpe): Update paths in winpe-SetupCompleteApplicationWorkspace to use temporary directory for AgentBootstrapper and log files

--- modules/winpe/functions.ps1
+++ modules/winpe/functions.ps1
@@ -601,8 +601,8 @@ function winpe-SetupCompleteApplicationWorkspace {
     param (
         [String]$agentbootstrapperURL = "https://download.liquit.com/extra/Bootstrapper/AgentBootstrapper-Win-2.1.0.2.exe",
         [String]$containerName = "liquit",
-        [String]$DestinationPath = "C:\ProgramData\Liquit\Agent",
-        [string]$logPath = "C:\ProgramData\Liquit\Agent",
+        [String]$DestinationPath = "C:\Windows\Temp",
+        [string]$logPath = "C:\Windows\Temp",
         [String]$storageAccountName = "madduxliquit",
         [switch]$StartDeployment = $false,
         [switch]$UseCertificate = $true
@@ -657,7 +657,7 @@ function winpe-SetupCompleteApplicationWorkspace {
 :: ========================================================
 :: Recast Software - Application Workspace
 :: ========================================================
-C:\ProgramData\Liquit\Agent\AgentBootstrapper.exe /certificate=C:\ProgramData\Liquit\Agent\AgentRegistration.cer /startDeployment /waitForDeployment /logPath=C:\ProgramData\Liquit\Agent"
+C:\Windows\Temp\AgentBootstrapper.exe /certificate=C:\Windows\Temp\AgentRegistration.cer /startDeployment /waitForDeployment /logPath=C:\Windows\Temp
 :: ========================================================
 "@
     $Content | Out-File -FilePath $SetupCompleteCmd -Append -Encoding ascii -Width 2000 -Force
