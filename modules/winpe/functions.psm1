43e0f28db0ed90e45095b63389d7135fb59982dc
David Segura <david@segura.org>
2026-01-16 22:24:14 -0600

    feat(winpe): add SendMessageTimeout function and Send-SettingChange for environment updates

--- modules/winpe/functions.psm1
+++ modules/winpe/functions.psm1
@@ -30,6 +30,19 @@ Functions are designed to be idempotent and can be safely re-run.
 Most functions will skip if the target is already configured/installed.
 #>
 
+Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
+  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+  public static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam, uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
+"@
+
+function Send-SettingChange {
+  $HWND_BROADCAST = [IntPtr] 0xffff;
+  $WM_SETTINGCHANGE = 0x1a;
+  $result = [UIntPtr]::Zero
+
+  [void] ([Win32.Nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [UIntPtr]::Zero, "Environment", 2, 5000, [ref] $result))
+}
+
 function winpe-SetExecutionPolicy {
     [CmdletBinding()]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '')]
